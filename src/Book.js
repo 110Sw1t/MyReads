79f8033820cab1e6b9d3dd2827f40e9ce964acff
AFoxWorks <dev.fassem@gmail.com>
2021-05-16 16:47:50 +0200

    Set default value if not provided for book thumbnail and shelf

--- src/Book.js
+++ src/Book.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { BOOK_SHELF } from './BooksAPI';
 import { SHELF } from './App';
+import GenricBookImage from './icons/generic-book.svg'
+
+
 /**
  * @constructor
  * @description Represents a book
@@ -20,9 +23,9 @@ class Book extends Component {
       return (
          (this.props.book && (<div className="book">
             <div className="book-top">
-               <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")` }}></div>
+               <div className="book-cover" style={{ "background-position":"center center", "background-repeat":"no-repeat", width: 128, height: 193, backgroundImage: `url("${(this.props.book.imageLinks && this.props.book.imageLinks.thumbnail) || GenricBookImage}")` }}></div>
                <div className="book-shelf-changer">
-                  <select value={this.props.book.shelf} onChange={this.changeShelf}>
+                  <select value={this.props.book.shelf || BOOK_SHELF.NONE} onChange={this.changeShelf}>
                      <option value="move" disabled>Move to...</option>
                      {Object.keys(BOOK_SHELF).map(status => {
                         let statusValue = BOOK_SHELF[status];
